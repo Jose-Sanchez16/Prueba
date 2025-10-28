0843cabd014341671da85216ae5d8641d15895f2
Jose-Sanchez16 <jose.sanchez.moreno.al@iespoligonosur.org>
2025-10-28 17:32:48 +0100

    Nuevo saludo informal

--- mi_proyecto/saludos.py
+++ mi_proyecto/saludos.py
@@ -1,8 +1,8 @@
 from utils.printer import print_message
 
 def saludo(name: str):
-    """Genera un saludo simple en español."""
-    message = f"Hola, {name}!"
+    """Genera un saludo informal."""
+    message = f"¡Hey {name}! ¿Qué tal?"
     print_message(message)
 
 def greet_in_english(name: str):
