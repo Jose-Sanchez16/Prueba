5ddcc8b1575e5c08cfcf89955519ad441af0c26b
Jose-Sanchez16 <jose.sanchez.moreno.al@iespoligonosur.org>
2025-10-28 16:57:57 +0100

    Mi primer codigo e python

---
+++ mi_proyecto/saludos.py
@@ -0,0 +1,6 @@
+from utils.printer import priny_message
+
+def saludo(name: str):
+    """Genera un saludo simple."""
+    message = f"Hola, {name}!"
+    print_message(message)
---
+++ mi_proyecto/utils/printer.py
@@ -0,0 +1,3 @@
+def priny_message(message: str):
+    """Imprime un mensaje en consola."""
+    print(message)
